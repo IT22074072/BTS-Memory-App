b0623830fa7c56e7f09b9f70f57fc7278857766c
IT22074072 <it22074072@my.sliit.lk>
2024-06-13 13:44:09 +0530

    Create Main UI with constraint layout

--- app/src/main/java/com/example/btsmemoryapp/MainActivity.kt
+++ app/src/main/java/com/example/btsmemoryapp/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.btsmemoryapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard:RecyclerView
+    private lateinit var tvNumMoves:TextView
+    private lateinit var tvNumPairs:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves= findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
